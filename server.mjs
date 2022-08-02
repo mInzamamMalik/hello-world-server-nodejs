940ee43850e0c406945145d19876cc727872b285
M.Inzamam Malik <malikasinger1@gmail.com>
2022-08-02 21:26:27 +0500

    minor change

--- server.mjs
+++ server.mjs
@@ -21,6 +21,8 @@ app.get('/weather', (req, res) => {
 
 })
 
-app.listen(3000, () => {
-  console.log(`Example app listening on port ${3000}`)
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
